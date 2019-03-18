8bb275f36902a15414ad4184b5e4689ded0ba579
greybutton <greybutton@yandex.com>
2019-03-18 11:03:59 +0800

    done fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,41 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        array = self
+        i = 0
+        while i < array.length
+          element = self[i]
+          yield(element)
+          i += 1
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        result = MyArray.new
+        my_each { |element| result << yield(element) }
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result = MyArray.new
+        my_each { |element| result << element unless element.nil? }
+        result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(total = nil)
+        array = self
+        i = 1
+        acc = self[0]
+        while i < array.length
+          element = self[i]
+          acc = yield(acc, element)
+          i += 1
+        end
+        total ? yield(total, acc) : acc
       end
     end
   end
